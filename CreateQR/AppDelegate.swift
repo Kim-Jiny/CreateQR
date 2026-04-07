6a12f672b1c116c6605d268c7e5573b38350cfd4
Jiny <jiny@nachocode.io>
2026-04-07 13:04:26 +0900

    가로모드 제거

--- CreateQR/AppDelegate.swift
+++ CreateQR/AppDelegate.swift
@@ -38,6 +38,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // MARK: - Orientation Lock
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 
 }
 
